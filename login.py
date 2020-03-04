e0ebca7c1787884fec2298dab53af866c1b1c1fd
jawniss <johnas@ualberta.ca>
2020-03-04 14:58:08 -0700

    Just started the login part

--- login.py
+++ login.py
@@ -1,9 +1,15 @@
 registeredOrNot = input("Are you registered? (Y/N)")
-if registered == Y
+print(registeredOrNot)
+if (registeredOrNot == "Y"):
     username = input("Please enter your username")
-    while username not in database
-        username = input("Invalid username, please try again")
-    password = input("Please enter your password")
-    while password not in database
-        password = input("Invalid password, please try again")
-else 
+    print(username)
+#     while username not in database
+#         username = input("Invalid username, please try again")
+#     password = input("Please enter your password")
+#     while password not in database
+#         password = input("Invalid password, please try again")
+# else 
+
+
+
+// for all the queries just put in comment block
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+#         username = input("Invalid username, please try again")
+#     password = input("Please enter your password")
+#     while password not in database
+#         password = input("Invalid password, please try again")
+# else 
