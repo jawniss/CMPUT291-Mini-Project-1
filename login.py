2b995a749de1921c490effce9ed362dbd17d6524
jawniss <johnas@ualberta.ca>
2020-03-04 14:42:29 -0700

    Starting login python file

---
+++ login.py
@@ -0,0 +1,9 @@
+registeredOrNot = input("Are you registered? (Y/N)")
+if registered == Y
+    username = input("Please enter your username")
+    while username not in database
+        username = input("Invalid username, please try again")
+    password = input("Please enter your password")
+    while password not in database
+        password = input("Invalid password, please try again")
+else 
