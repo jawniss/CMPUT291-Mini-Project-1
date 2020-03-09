b370fd12c339faed11f313137acf8bcca808bb76
Rafaella Graña <graa@ualberta.ca>
2020-03-08 21:10:06 -0600

    created main skeleton

---
+++ main.py
@@ -0,0 +1,35 @@
+from getpass import getpass #hidden input function
+
+def selectLoginOption:
+    print("Please select your login options\n")
+    print("1. Log in: (Already have an account)\n")
+    print("2. Sign up: (Create new account)\n")
+    input(signup)
+    if (signup== "1" or signup=="2"):
+        return (True,signup)
+    else:
+        return(False,"")
+
+
+if __name__="__main__":
+
+    print("welcome to our miniproject\n")
+    loginoptionselected=False
+    #choose login option
+    while loginoptionselected:
+        print("Please select your login options\n")
+        print("1. Log in: (Already have an account)\n")
+        print("2. Sign up: (Create new account)\n")
+        input(signup)
+    #log in
+    #or sign up
+    login = False;
+    while not login:
+        try:
+            #ask for username and password
+        except:
+    while 1:
+        #show options
+        #get user input
+        #check for attack
+        # 
